f5ecf491603eea70b0373261611403424d29b548
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-09-05 15:01:38 +0300

    Add Time evaluation extension.

---
+++ EMonolithLib/EMonolithLib/Extensions/Timing/TimeEvaluateExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMonolithLib.Extensions.Timing
+{
+    public static class TimeEvaluateExtensions
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static T EvaluateTime<T>(this object context, Func<T> function, out double EvaluatedTimeMS)
+        {
+            _ = context;
+
+            stopwatch.Restart();
+
+            T result = function();
+
+            stopwatch.Stop();
+
+            EvaluatedTimeMS = stopwatch.Elapsed.TotalMilliseconds;
+
+            return result;
+        }
+    }
+}
